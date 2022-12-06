4fc9f7c56965c920a7cfdace82057fd797834500
casp2009 <56613238+casp2009@users.noreply.github.com>
2022-12-06 08:58:57 -0300

    Delete envio-mensagens directory

--- envio-mensagens/src/main/java/com/manoelcampos/message/MessageSendException.java
+++
@@ -1,9 +0,0 @@
-package com.manoelcampos.message;
-
-/**
- * Exceção para indicar que houve erro ao tentar enviar uma mensagem por um {@link MessageService}.
- * @author Manoel Campos da Silva Filho
- */
-public class MessageSendException extends RuntimeException {
-
-}
